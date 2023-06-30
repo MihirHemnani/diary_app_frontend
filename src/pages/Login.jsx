9e0cfc20b63d2af5d6c76ef3028692b45d8c8e3a
Hemnani Mihir Jitumal <mihir.hemnani2336.gmail.com>
2023-06-30 14:50:27 +0530

    spinner margin

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -24,78 +24,82 @@ const Login = () => {
     return (
         <>
             <Navbar />
-            {loader && <Spinner />}
-            <div className="container">
+            {loader
+                ?
+                <Spinner />
+                :
+                <div className="container">
 
-                <section className="section register min-vh-100 d-flex flex-column align-items-center justify-content-center">
-                    <div className="container" style={{ marginTop: "5rem", marginBottom: "3rem" }}>
-                        <div className="row justify-content-center">
-                            <div className="col-lg-4 col-md-6  d-flex flex-column align-items-center justify-content-center">
+                    <section className="section register min-vh-100 d-flex flex-column align-items-center justify-content-center">
+                        <div className="container" style={{ marginTop: "5rem", marginBottom: "3rem" }}>
+                            <div className="row justify-content-center">
+                                <div className="col-lg-4 col-md-6  d-flex flex-column align-items-center justify-content-center">
 
-                                {/* <div className="d-flex justify-content-center py-4">
+                                    {/* <div className="d-flex justify-content-center py-4">
                                     <a href="index.html" className="logo d-flex align-items-center w-auto">
                                         <span className="d-none d-lg-block">NiceAdmin</span>
                                     </a>
                                 </div> */}
 
-                                <div className="card">
+                                    <div className="card">
 
-                                    <div className="card-body" style={{ textDecoration: 'none' }}>
+                                        <div className="card-body" style={{ textDecoration: 'none' }}>
 
-                                        <div className="pt-4 pb-2">
-                                            <h5 className="card-title text-center pb-0 fs-4">Login to Your Account</h5>
+                                            <div className="pt-4 pb-2">
+                                                <h5 className="card-title text-center pb-0 fs-4">Login to Your Account</h5>
+                                            </div>
+
+                                            <form className="row g-3 needs-validation" onSubmit={handleSubmit(onSubmit)}>
+
+                                                <div className="col-12">
+                                                    <label htmlFor="yourEmail" className="form-label">Email</label>
+                                                    <div className="input-group has-validation">
+                                                        <input type="email"
+                                                            autoComplete="off"
+                                                            {...register("email", { required: 'required field' })}
+                                                            className="form-control" id="yourEmail" />
+                                                    </div>
+                                                    <p>{errors.email?.message}</p>
+                                                </div>
+
+                                                <div className="col-12">
+                                                    <label htmlFor="yourPassword" className="form-label">Password</label>
+                                                    <div className="input-group has-validation">
+                                                        <input type="password"
+                                                            autoComplete="off"
+                                                            {...register("password", { required: 'required field' })}
+                                                            className="form-control" id="yourPassword" />
+                                                    </div>
+                                                    <p>{errors.password?.message}</p>
+                                                </div>
+
+                                                <div className="col-12">
+                                                    <p className="small mb-1 mt-0">Forget Password? <Link to="/api/password_reset_link">Click Here</Link></p>
+                                                </div>
+
+                                                <div className="col-12">
+                                                    <button className="btn btn-primary w-100" type="submit">Login</button>
+                                                </div>
+                                                <div className="col-12">
+                                                    <p className="small mb-0">Don't have account? <Link to="/api/register">Create an account</Link></p>
+                                                </div>
+                                            </form>
+
                                         </div>
-
-                                        <form className="row g-3 needs-validation" onSubmit={handleSubmit(onSubmit)}>
-
-                                            <div className="col-12">
-                                                <label htmlFor="yourEmail" className="form-label">Email</label>
-                                                <div className="input-group has-validation">
-                                                    <input type="email"
-                                                        autoComplete="off"
-                                                        {...register("email", { required: 'required field' })}
-                                                        className="form-control" id="yourEmail" />
-                                                </div>
-                                                <p>{errors.email?.message}</p>
-                                            </div>
-
-                                            <div className="col-12">
-                                                <label htmlFor="yourPassword" className="form-label">Password</label>
-                                                <div className="input-group has-validation">
-                                                    <input type="password"
-                                                        autoComplete="off"
-                                                        {...register("password", { required: 'required field' })}
-                                                        className="form-control" id="yourPassword" />
-                                                </div>
-                                                <p>{errors.password?.message}</p>
-                                            </div>
-
-                                            <div className="col-12">
-                                                <p className="small mb-1 mt-0">Forget Password? <Link to="/api/password_reset_link">Click Here</Link></p>
-                                            </div>
-
-                                            <div className="col-12">
-                                                <button className="btn btn-primary w-100" type="submit">Login</button>
-                                            </div>
-                                            <div className="col-12">
-                                                <p className="small mb-0">Don't have account? <Link to="/api/register">Create an account</Link></p>
-                                            </div>
-                                        </form>
-
                                     </div>
-                                </div>
 
-                                {/* <div className="credits">
+                                    {/* <div className="credits">
                                     Designed by Mihir
                                 </div> */}
 
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                </section>
+                    </section>
 
-            </div>
+                </div>
+            }
             <Footer />
         </>
     )
